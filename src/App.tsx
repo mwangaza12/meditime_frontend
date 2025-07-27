7679223d1c46317f6dd2c7ec7ebd128926345977
mwangaza12 <josephmwangaza1@gmail.com>
2025-07-27 15:56:00 +0300

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 function App() {
   useEffect(() => {
     console.log("VITE_BACKEND_URL:", import.meta.env.VITE_BACKEND_URL)
+    console.log("VITE_BACKEND_URL:", import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY)
   }, [])
 
   return (
