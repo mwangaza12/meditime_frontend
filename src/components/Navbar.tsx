f4eee4c07a913dcd975313ecf58f37a9b40d49e3
mwangaza12 <josephmwangaza1@gmail.com>
2025-09-03 15:49:28 +0300

    refactor: z index fix in the navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
     '/user-dashboard';
 
   return (
-    <nav className="bg-white shadow-lg sticky top-0 z-50">
+    <nav className="bg-white shadow-lg sticky top-0 z-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
